c6347cdf06521864e5111dd566c4256705981292
Oliver Graf <gra17190@uvg.edu.gt>
2019-03-29 19:28:28 -0600

    Funciona Version de JCF

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,52 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        //PriorityQueue<Paciente> q = new PriorityQueue<>();
+
+        PriorityQueue<Paciente> q = new PriorityQueue<>();
+        Scanner input = new Scanner(System.in);
+        Boolean go = false;
+
+        System.out.println("Se ha iniciado el programa. Bienvenido a la cola de prioridad.");
+        do{
+            System.out.println("Ingrese la direccion del archivo como se muestra en el ejemplo: C:\\\\Users\\\\ejemplo\\\\Desktop\\\\test.txt");
+            String direccion = input.nextLine();
+
+            try {
+                File f = new File(direccion);
+                BufferedReader b = new BufferedReader(new FileReader(f));
+                String readLine;
+                System.out.println("Pacientes encontradoes en el archivo especificado\n");
+                while ((readLine = b.readLine()) != null) {
+                    Paciente p = new Paciente();
+                    String[] entradas = readLine.split(", ");
+                    p.setNombre(entradas[0]);
+                    p.setSintoma(entradas[1]);
+                    p.setCodigo(entradas[2]);
+                    System.out.println(p.toString());
+                    q.add(p);
+                }
+                System.out.println("El priority queue fue llenado exitosamente.");
+                go = true;
+            } catch (Exception e) {
+                System.out.println("No fue posible la lectura del archivo con la direccion que especifico...\nIntentelo de nuevo!");
+            }
+                System.out.println("La lectura del archivo fue exitosa");
+
+        }while(!go);
+        System.out.println("En seguida se despliegara en pantalla la lista ordenada de pacientes: \n");
+        while(!q.isEmpty()){
+            System.out.println(q.remove().toString());
+        }
+
+
+
 
     }
 }
--- src/Paciente.java
+++ src/Paciente.java
@@ -1,8 +1,14 @@
-public class Paciente<T> implements Comparable<T> {
+public class Paciente implements Comparable<Paciente> {
 
     private String nombre;
     private String sintoma;
     private String codigo;
+
+    public Paciente() {
+        nombre = "";
+        sintoma = "";
+        codigo = "";
+    }
 
     public String getNombre() {
         return nombre;
@@ -16,8 +22,29 @@ public class Paciente<T> implements Comparable<T> {
         return codigo;
     }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setSintoma(String sintoma) {
+        this.sintoma = sintoma;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
     @Override
-    public int compareTo(T o) {
-        return 0;
+    public String toString() {
+        return "Paciente{" +
+                "nombre='" + nombre + '\'' +
+                ", sintoma='" + sintoma + '\'' +
+                ", codigo='" + codigo + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Paciente p) {
+        return this.codigo.compareTo(p.getCodigo());
     }
 }
